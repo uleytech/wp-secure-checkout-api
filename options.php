0dc5b2d07e1c576f0cdd1f29de31aecadb28a21b
Oleksandr Krokhin <alexander@krohin.com>
2020-09-23 15:45:52 +0300

    add options to api 2

--- options.php
+++ options.php
@@ -24,7 +24,7 @@ function register_settings()
 {
     register_setting('wp_secure_checkout_api_options', 'wp_secure_checkout_api_options', 'wp_secure_checkout_api_options_validate');
     add_settings_section('api_settings', 'ID Settings', 'wp_secure_checkout_api_section_text', 'wp_secure_checkout_api');
-    add_settings_field('wp_init_affiliate_id_setting_api_key', 'API Key', 'wp_secure_checkout_api_setting_api_key', 'wp_secure_checkout_api', 'api_settings');
+    add_settings_field('wp_secure_checkout_api_setting_api_key', 'API Key', 'wp_secure_checkout_api_setting_api_key', 'wp_secure_checkout_api', 'api_settings');
 }
 
 add_action('admin_init', 'register_settings');
@@ -38,12 +38,12 @@ function wp_secure_checkout_api_options_validate($input)
     return $newinput;
 }
 
-function wp_init_affiliate_id_section_text()
+function wp_secure_checkout_api_section_text()
 {
     echo '<p>Here you can set all the options for using the Secure Checkout API</p>';
 }
 
-function wp_init_affiliate_id_setting_api_key()
+function wp_secure_checkout_api_setting_api_key()
 {
     $options = get_option('wp_secure_checkout_api_options');
     echo "<input id='wp_secure_checkout_api_setting_api_key' name='wp_secure_checkout_api_options[api_key]' type='text' value='" . esc_attr($options['api_key']) . "' />";
